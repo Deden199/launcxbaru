2d97993a1e9a216d744b3eb48a80f88cf692a338
Nail <92734420+Deden199@users.noreply.github.com>
2025-08-25 01:43:02 +0700

    Include admin names in admin logs

--- src/util/adminLog.ts
+++ src/util/adminLog.ts
@@ -6,9 +6,15 @@ export async function logAdminAction(
   target?: string | null,
   detail?: any
 ) {
+  const admin = await prisma.partnerUser.findUnique({
+    where: { id: adminId },
+    select: { name: true },
+  })
+
   await prisma.adminLog.create({
     data: {
       adminId,
+      adminName: admin?.name || '',
       action,
       target,
       detail,
